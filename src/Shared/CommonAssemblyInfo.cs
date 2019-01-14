f83582281f308a1f0fa91a8cb6e26b5ce3d8fe7f
Benn Benson <madreflection@live.com>
2019-01-14 01:39:00 -0800

    Minor version bump.

--- src/Shared/CommonAssemblyInfo.cs
+++ src/Shared/CommonAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
-[assembly: AssemblyInformationalVersion("0.0.1")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
